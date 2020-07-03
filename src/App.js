2957b8502dad3699831712ac090d7636c27341a0
Pronnoy <pronnoy1998@gmail.com>
2020-07-03 23:08:29 +0530

    Created the frontend of the app

---
+++ src/App.js
@@ -0,0 +1,155 @@
+import React, { Component } from 'react';
+import Particles from 'react-particles-js'
+import Clarifai from 'clarifai';
+import Navigation from './Components/Navigation/Navigation'
+import SignIn from './Components/SignIn/SignIn'
+import Logo from './Components/Logo/Logo'
+import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm'
+import Rank from './Components/Rank/Rank'
+import Register from './Components/Register/Register'
+import FaceRecognition from './Components/FaceRecognition/FaceRecognition'
+import './App.css';
+
+const app = new Clarifai.App({
+  apiKey: "9215d097ec2440a0892ed4f270dfd74c",
+});
+
+const particlesOptions = {
+  particles: {
+    number: { value: 80, density: { enable: true, value_area: 800 } },
+    color: { value: "#b6ff07" },
+    shape: {
+      type: "star",
+      stroke: { width: 0, color: "#000000" },
+      polygon: { nb_sides: 10 },
+      image: { src: "img/github.svg", width: 100, height: 100 }
+    },
+    opacity: {
+      value: 0.5,
+      random: false,
+      anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false }
+    },
+    size: {
+      value: 0,
+      random: true,
+      anim: { enable: true, speed: 40, size_min: 0.1, sync: false }
+    },
+    line_linked: {
+      enable: true,
+      distance: 249.21083236417988,
+      color: "#ffffff",
+      opacity: 0.4,
+      width: 1
+    },
+    move: {
+      enable: true,
+      speed: 6,
+      direction: "none",
+      random: false,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+      attract: { enable: false, rotateX: 600, rotateY: 1200 }
+    }
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: { enable: true, mode: "repulse" },
+      onclick: { enable: true, mode: "push" },
+      resize: true
+    },
+    modes: {
+      grab: { distance: 400, line_linked: { opacity: 1 } },
+      bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
+      repulse: { distance: 200, duration: 0.4 },
+      push: { particles_nb: 4 },
+      remove: { particles_nb: 2 }
+    }
+  },
+  retina_detect: true
+};
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      input: '',
+      imageUrl: '',
+      box:{},
+      route:'signin', 
+      isSignedIn: false
+    }
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    this.setState({ box: box });
+  }
+
+  onInputChange = (event) => {
+    this.setState({ input: event.target.value });
+  }
+
+  onButtonSubmit = () =>{
+    this.setState({imageUrl : this.state.input});
+    app.models
+      .predict(
+        'c0c0ac362b03416da06ab3fa36fb58e3',
+        this.state.input)
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .catch(err => console.log(err))  
+  }
+
+  onRouteChange= (route) =>{
+    if(route=== 'signout'){
+      this.setState({isSignedIn : false})
+    } else if (route === 'home'){
+      this.setState({ isSignedIn: true });
+    }
+    this.setState({route:route});
+  }
+
+  render(){
+    const { isSignedIn, imageUrl, route, box } = this.state
+    return (
+    <div className="App">
+      <Particles className="particles"
+        params={particlesOptions}
+      />
+      <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange}/>
+      { route ==="home" ?
+          <div>
+            <Logo />
+            <Rank />
+            <ImageLinkForm
+              onInputChange={this.onInputChange}
+              onButtonSubmit={this.onButtonSubmit} />
+            <FaceRecognition box={box} imageUrl={imageUrl} />
+          </div>
+      : (
+        this.state.route ==='signin' ? 
+              <SignIn onRouteChange={this.onRouteChange} /> 
+        :     <Register onRouteChange={this.onRouteChange} /> 
+              
+      )
+          
+      }
+    </div>
+  );
+  }
+}
+
+export default App;
