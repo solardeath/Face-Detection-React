35bc44c77f1fcae6ad860c7949f0539fd8620e35
Pronnoy <pronnoy1998@gmail.com>
2020-07-05 20:45:52 +0530

    Added register and Rank functionality

--- src/Components/SignIn/SignIn.js
+++ src/Components/SignIn/SignIn.js
@@ -27,11 +27,12 @@ class SignIn extends React.Component {
             })
         })
         .then(response => response.json())
-        .then(data => {
-            if (data === 'Success') {
+        .then(user => {
+            if (user.id) {
+                this.props.loadUser(user);
                 this.props.onRouteChange('home');
             }
-        })     
+        })   
     }
 
 
