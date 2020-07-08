88ecf60a653957978241d5a719651cdd47c4c509
Pronnoy <pronnoy1998@gmail.com>
2020-07-08 13:34:02 +0530

    Deploy

--- src/App.js
+++ src/App.js
@@ -120,7 +120,7 @@ class App extends Component {
 
   onButtonSubmit = () =>{
     this.setState({imageUrl : this.state.input});
-    fetch("http://localhost:3000/imageurl", {
+    fetch("https://lit-refuge-72599.herokuapp.com/imageurl", {
       method: 'post',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
@@ -130,7 +130,7 @@ class App extends Component {
     .then(response =>response.json())
     .then(response => {
         if(response){
-          fetch("http://localhost:3000/image", {
+          fetch("https://lit-refuge-72599.herokuapp.com/image", {
             method: 'put',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
